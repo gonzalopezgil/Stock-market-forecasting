cb933b049fd461b752acb14b3e9977ff520dd34a
Gonzalo López Gil <gonzalo.lopezg@edu.uah.es>
2024-06-23 18:07:45 +0200

    feat: gpu compatibility for all models

--- NextDay-240,1-LSTM.py
+++ NextDay-240,1-LSTM.py
@@ -22,6 +22,19 @@ random.seed(SEED)
 np.random.seed(SEED)
 tf.set_random_seed(SEED)
 
+# Setting up GPU configuration
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])  # set memory limit if necessary
+    except RuntimeError as e:
+        print(e)
+
 SP500_df = pd.read_csv('data/SPXconst.csv')
 all_companies = list(set(SP500_df.values.flatten()))
 all_companies.remove(np.nan)
